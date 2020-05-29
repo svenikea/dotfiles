0df96892ab435cdd1694eba1009988bf78a48d8b
kynguyen98 <nguyenhongky1998@gmail.com>
2020-05-29 22:54:08 +0700

    new way to install

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #! /usr/bin/sh
 
-# Initial package before this can work
-install python-pip yarn
-pip install jedi pynvim 
+# Installing vim plug currently using neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Installing NodeJs
+curl -sL install-node.now.sh/lts | bash
+
+# Package before this can work
+install python-pip yarn xclip
+pip install jedi pynvim
+
+# Add neovim to yarn at global env
 yarn global add neovim
 
-
+# Moving files 
 cp .config/nvim $HOME/.config
 cp .bash_aliases .bash_profile .bashrc .xinitrc $HOME/
