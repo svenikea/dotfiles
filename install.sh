ee3f902bf9928d41b6e25f2ff3d512cd0dfcf918
john <nguyenhongky1998@gmail.com>
2020-06-07 12:24:26 -0700

    new install

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #! /usr/bin/sh
 
 # Moving files 
-cp .config/nvim $HOME/.config
+cp .config $HOME/
 cp .bash_aliases .bash_profile .bashrc .xinitrc $HOME/
 
 # Installing vim plug currently using neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Installing NodeJs
-sudo curl -sL install-node.now.sh/lts | bash
 
 # Package before this can work
-install python-pip yarn xclip
+install python-pip yarn xclip nodejs  
 pip install jedi pynvim
 
 # Add neovim to yarn at global env
 yarn global add neovim
 
+# Lastly change the permission 
+chown john $HOME/.config
