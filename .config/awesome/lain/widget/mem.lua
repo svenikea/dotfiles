f1a9f4941f8562e7af846eee5011354f7ef59303
john <nguyenhongky1998@gmail.com>
2020-08-04 09:40:05 -0700

    small changes

--- .config/awesome/lain/widget/mem.lua
+++ .config/awesome/lain/widget/mem.lua
@@ -5,20 +5,23 @@
       * (c) 2010-2012, Peter Hofmann
 
 --]]
-
+local gears 		   = require ("gears")
+local beautiful 	   = require("beautiful")
 local helpers              = require("lain.helpers")
 local wibox                = require("wibox")
 local gmatch, lines, floor = string.gmatch, io.lines, math.floor
-
+local lain = require("lain")
+local markup = lain.util.markup
 -- Memory usage (ignoring caches)
 -- lain.widget.mem
-
 local function factory(args)
     local mem      = { widget = wibox.widget.textbox() }
     local args     = args or {}
-    local timeout  = args.timeout or 2
-    local settings = args.settings or function() end
-
+    local icon_dir = os.getenv("HOME") .. "/.config/awesome/themes/zenburn/icons/"
+    local timeout  = args.timeout or 1
+    local settings = args.settings or function() end 
+    mem.icon_path = icon_dir .. "mem.png"
+    mem.icon = wibox.widget.imagebox(mem.icon_path)
     function mem.update()
         mem_now = {}
         for line in lines("/proc/meminfo") do
@@ -37,14 +40,15 @@ local function factory(args)
         mem_now.used = mem_now.total - mem_now.free - mem_now.buf - mem_now.cache - mem_now.srec
         mem_now.swapused = mem_now.swap - mem_now.swapf
         mem_now.perc = math.floor(mem_now.used / mem_now.total * 100)
-
+	if (mem_now.perc < 29 ) then mem_now.color = "#00ff00"
+	elseif (mem_now.perc > 30 and mem_now.perc < 69) then mem_now.color = "#ffff00"
+	elseif (mem_now.perc > 70) then mem_now.color = "#ff0000" 
+	end
         widget = mem.widget
         settings()
+	widget:set_markup(markup(mem_now.color, mem_now.used) .. markup("#ffff00" ,"M"))
     end
-
     helpers.newtimer("mem", timeout, mem.update)
-
     return mem
 end
-
 return factory
